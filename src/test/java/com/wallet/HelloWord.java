5a21dc760605ca19d7566e81c3ff51244bfb1e70
Robson Murilo <robsonmurilo.f@gmail.com>
2020-03-04 17:35:22 -0300

    configuração inicial de testes

---
+++ src/test/java/com/wallet/HelloWord.java
@@ -0,0 +1,19 @@
+package com.wallet;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.swing.*;
+
+import static org.junit.Assert.assertEquals;
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HelloWord {
+
+    @Test
+    public void testeWelloWord(){
+        assertEquals(1,2);
+
+    }
+}
