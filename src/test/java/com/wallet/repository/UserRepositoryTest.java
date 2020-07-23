cd4f9cfd362790ad3ea7e00645a39bb45feac059
Robson Murilo <robsonmurilo.f@gmail.com>
2020-07-23 18:00:38 -0300

    adicionando metodo de pesquisa por email

--- src/test/java/com/wallet/repository/UserRepositoryTest.java
+++ src/test/java/com/wallet/repository/UserRepositoryTest.java
@@ -2,20 +2,47 @@ package com.wallet.repository;
 
 
 import com.wallet.entity.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import static org.junit.Assert.assertNotNull;
+
+import javax.swing.text.html.Option;
+
+import java.util.Optional;
+
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserRepositoryTest {
 
+    public static String email="casa@casa.com";
+
 
     @Autowired
     UserRepository repository;
+
+    @Before
+    public void setUp(){
+        User u = new User();
+        u.setNome("Robson");
+        u.setEmail(email);
+        u.setPassword("123");
+
+        repository.save(u);
+
+    }
+
+    @After
+    public void terDown(){
+        repository.deleteAll();
+
+
+    }
 
     @Test
     public void testSave(){
@@ -25,7 +52,16 @@ public class UserRepositoryTest {
         u.setEmail("jose@jose.com");
 
         User responde = repository.save(u);
+        System.out.println(responde.getNome());
 
         assertNotNull(responde);
+    }
+
+    public void testFindByEmail(){
+
+        Optional<User>responde = repository.findByEmail(email);
+        assertTrue(responde.isPresent());
+        assertEquals(responde.get().getEmail(),email);
+
     }
 }
